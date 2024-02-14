75291b5e3f1445caaaddd842acace4cd93d4a7c2
shivam Ahir <11shivam00@gmail.com>
2024-02-14 18:41:19 +0530

    home page complete

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -227,7 +227,7 @@ const Home = () => {
         </Stack>
         <Stack sx={{bgcolor:'gray',color:'white'}}>
             <Stack direction={'row'} sx={{display:'flex',justifyContent:'center',marginTop:'40px',marginBottom:'30px'}} gap={10}>
-                <Stack sx={{width:'500px'}} gap={3}>
+                <Stack sx={{width:'500px'}} gap={5}>
                     <Stack direction={'row'} gap={2}>
                     <Box sx={{width:"50px", height:"50px"}} > 
                     <DinnerDiningOutlinedIcon sx={{ width: "100%", height: "100%" }}  />
@@ -241,7 +241,7 @@ const Home = () => {
                             In the era of new Technology we look a future with certainity and pride to our company
                         </Typography>
                     </Stack>
-                    <Stack direction={'row'} gap={2}>
+                    <Stack direction={'row'} gap={5}>
                         <Avatar style={{ backgroundColor: '#ff0000' }}>
                             <FacebookOutlinedIcon />
                         </Avatar>
@@ -259,29 +259,46 @@ const Home = () => {
                     <Typography variant='h4' sx={{fontWeight:'600'}}>
                         Pages
                     </Typography>
-                    <Typography variant='h5'>
+                    <Typography variant='h6'>
                         Home
                     </Typography>
-                    <Typography variant='h5'>
+                    <Typography variant='h6'>
                         About
                     </Typography>
-                    <Typography variant='h5'>
+                    <Typography variant='h6'>
                         Menu
                     </Typography>
-                    <Typography variant='h5'>
+                    <Typography variant='h6'>
                         Contact
                     </Typography>
                 </Stack>
-                <Stack sx={{width:'500px'}}>
+                <Stack sx={{width:'500px'}} gap={3}>
                     <Typography variant='h4' sx={{fontWeight:'600'}}>
                         Follow Us On Instagram
                     </Typography>
-                    <Stack>
-
+                   <Stack  gap={3}>
+                   <Stack direction={'row'} gap={2}>
+                        <Stack sx={{width:'250px'}}>
+                            <img src='https://img.freepik.com/premium-photo/french-fries-sweet-potatoes-with-onion_97105-132.jpg?w=360'/>
+                        </Stack>
+                        <Stack sx={{width:'230px'}}>
+                            <img src='https://content.jdmagicbox.com/comp/def_content/pizza_outlets/default-pizza-outlets-7.jpg'/>
+                        </Stack>
                     </Stack>
+                    <Stack direction={'row'} gap={2}>
+                        <Stack sx={{width:'250px'}}>
+                            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7BrLBlr5f3E-OfQ8qcp7_wQuraImR5yMxBt4JfSD6ora5ODo_QQCdVH2FCp_ZvRrFheU&usqp=CAU'/>
+                        </Stack>
+                        <Stack sx={{width:'230px'}}>
+                            <img src='https://media-assets.swiggy.com/swiggy/image/upload/f_auto,q_auto,fl_lossy/fbbacd06e85b9459cc1cb411b07a00ec'/>
+                        </Stack>
+                    </Stack>
+                   </Stack>
                 </Stack>
             </Stack>
-            <Stack direction={'row'} gap={1} sx={{display:'flex',justifyContent:'center'}}>
+            <Stack direction={'row'} gap={1} sx={{display:'flex',justifyContent:'center',marginBottom:'20px',marginTop:'20px'}}>
+ 
+                <Stack direction={'row'} gap={1}>
                 <Typography>
                     Copyright 
                 </Typography>
@@ -289,6 +306,7 @@ const Home = () => {
                 <Typography>
                     Emporium. All Right Reserved
                 </Typography>
+                </Stack>
             </Stack>
         </Stack>
     </Box>
